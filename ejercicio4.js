dbd97f51d769f823d941ffb83c64e1389d7979d0
NOZZAJ777 <jnossapoveda10@gmail.com>
2025-11-10 11:35:26 -0500

    ejercicio 4 Nossa

---
+++ ejercicio4.js
@@ -0,0 +1,15 @@
+//elaborar un algoritmo que muestre el promedio de las edades de la ficha y cuantos son mayores de edad
+
+function promedio(Aprendices, edad, i){
+    i=''
+    while (Aprendices<i) {
+        edad+= edad;
+        if (edad>=18) {
+            console.log("el aprendiz es mayor de edad")     
+        }
+        else
+            console.log("el aprendiz NO es mayor de edad")  
+    }
+
+}
+
