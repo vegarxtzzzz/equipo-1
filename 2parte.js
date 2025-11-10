8d327f281cca5a74fccce2375ac88a19cc852e9f
NikolOrtiz <tatisnik1022@gmail.com>
2025-11-10 11:51:49 -0500

    Add files via upload

---
+++ 2parte.js
@@ -0,0 +1,14 @@
+//elabore un algoritmo que muestre el total pagado de una compra con x cantidad de productos.
+//  Aparte del total deben mostrar el iva con descuento del 5% si la compra es superio a 100.000,la prsona paga e efectivo por ende debe mostrar cuales so las vueltas para el usuario.
+function total(productos,iva,total);
+total = "";
+iva = 5;
+Productos= X;
+if Producto(>=100){
+  print ("su cantidad de produtos es :" + productos)
+
+esle 
+  print ("No mostrar cantidad de productos")
+
+}
+print ("El resultado de las vueltas es :")
