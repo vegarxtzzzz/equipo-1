091df108fce281f266d48c6d05d259c9aa8afb3d
camiloc8u <camilomoralesg251@gmail.com>
2025-11-10 11:08:18 -0500

    Add files via upload

---
+++ Camilo Morales reto 1/a.js
@@ -0,0 +1,14 @@
+//calcular el triangulo
+
+function triangulo (area1, area2, area3,re) {
+    area1 = num1 * num2;
+    area2 = num1 * num2;
+    area3=  num1 * num2;
+    re= num1 + 
+}
+
+let num1;
+let num2;
+let num3;
+
+prompt = (inputfloat("Digite las medidas en cm"))
