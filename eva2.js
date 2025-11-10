83804b9ed025bad29e80d96fadfe17f80957ccbf
bryamziuuu <respaldoc998@gmail.com>
2025-11-10 11:51:22 -0500

    2. Hacer una funcion que muestre el total pagado de una compra con X cantidad de productos. Aparte del total, debe mostrar el IVA

---
+++ eva2.js
@@ -0,0 +1,21 @@
+// Hacer una funcion que muestre el total pagado de una compra con X cantidad de productos. Aparte del total, debe mostrar el IVA
+// un descuento del 5% si la compra es superior a 100k, la persona en efectivo por ende debe de mostrar las vueltas para el usuario
+
+function totalPagado(productos){
+    let IVA;
+    let descuento;
+    let total;
+
+    cantidadProductos=IVA-descuento+total
+
+if (total>100){
+    total  * IVA
+} 
+    
+    efectivo-total;
+
+descuento
+
+ console.log ("sus vueltas son", total)
+
+}
