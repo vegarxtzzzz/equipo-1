deb3b713f2b01b27e4fb27fea78bf13c437a52c9
NOZZAJ777 <jnossapoveda10@gmail.com>
2025-11-10 11:23:49 -0500

    Ejercicio 2 Nossa

---
+++ EvaluacionNossa 10-11/ejercicio nossa2.js
@@ -0,0 +1,7 @@
+function multiplicacion(num1, num2, ope){
+        ope = num1 * num2;
+}
+let Calcular = multiplicacion(num1, num2, ope)
+num1 =prompt(String("Digite el primer numero"));
+Num2 = prompt(String("Digite el segundo numero"));
+console.log("El resultado es: " + Calcular);
---
+++ EvaluacionNossa 10-11/ejercicio.js
@@ -0,0 +1,14 @@
+//calcular el triangulo
+
+function Caclcular(lado1, lado2, lado3){
+
+    let lado1 = 
+}
+
+
+triangulo = Caclcular(lado1, lado2, lado3)
+
+
+prompt (float())
+console.log
+
