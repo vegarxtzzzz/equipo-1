4a879662c8d49af84d37067d1f238034e6c9c2bb
camiloc8u <camilomoralesg251@gmail.com>
2025-11-10 11:23:33 -0500

    Add files via upload

---
+++ a.js
@@ -0,0 +1,10 @@
+//mostrar la multipicacion digitado
+
+function mul(num1, num2) {
+
+    num1= promt parseFloat("Digite numero 1 para multiplicar");
+    num2= promt parseFloat("Digite numero 2 para multiplicar"); 
+    
+    m = num1* num2;
+    return m;
+}
